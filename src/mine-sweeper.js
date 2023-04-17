82f900f2faab2d1045e5917e627e92af16c45166
Александр Попов <agpopov@t1-consulting.ru>
2023-04-17 19:08:26 +0300

    feat: add resolve of mac-address, mine-sweeper and sum-digits

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+//const { NotImplementedError } = require('../extensions/index.js');
 
 /**
  * In the popular Minesweeper game you have a board with some mines and those cells
@@ -23,11 +23,53 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+    //throw new NotImplementedError('Not implemented');
+    const resultArr = Array.from(Array(matrix.length), () => new Array(matrix[0].length));
+
+    for (let i = 0; i < matrix.length; i++) {
+        for (let j = 0; j < matrix[i].length; j++) {
+            let count = 0;
+            const diagTopLeftEl = (i, j) => matrix[i - 1][j - 1];
+            const diagTopRightEl = (i, j) => matrix[i - 1][j + 1];
+
+            const diagBottomLeftEl = (i, j) => matrix[i + 1][j - 1];
+            const diagBottomRightEl = (i, j) => matrix[i + 1][j + 1];
+
+            const topEl = (i, j) => matrix[i - 1][j];
+            const bottomEl = (i, j) => matrix[i + 1][j];
+            const leftEl = (i, j) => matrix[i][j - 1];
+            const rightEl = (i, j) => matrix[i][j + 1];
+
+            if (i > 0) {
+                if (j > 0 && diagTopLeftEl(i, j)) count++;
+                if (topEl(i, j)) count++;
+                if (j <= matrix[0].length && diagTopRightEl(i, j)) count++;
+                if (j <= matrix[0].length - 1 && rightEl(i, j)) count++;
+            }
+            if (j > 0) {
+                if (leftEl(i, j)) count++;
+                if (i < matrix.length - 1 && diagBottomLeftEl(i, j)) count++;
+                if (i < matrix.length - 1 && bottomEl(i, j)) count++;
+            }
+            if (i < matrix.length - 1) {
+                if (j < matrix[0].length - 1 && diagBottomRightEl(i, j)) count++
+            }
+
+            resultArr[i][j] = count
+        }
+    }
+    return resultArr;
 }
 
+// matrix = [
+//     [true, false, false],
+//     [false, true, false],
+//     [false, false, false]
+// ]
+//
+// console.log(minesweeper(matrix))
+
 module.exports = {
-  minesweeper
+    minesweeper
 };
--- src/sum-digits.js
+++ src/sum-digits.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+//const {NotImplementedError} = require('../extensions/index.js');
 
 /**
  * Given a number, replace this number with
@@ -12,11 +12,15 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
  *
  */
-function getSumOfDigits(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSumOfDigits(n) {
+    //throw new NotImplementedError('Not implemented');
+    const sumOfDigits = n.toString()
+        .split("")
+        .reduce((prevValue, currValue) => Number(prevValue) + Number(currValue));
+    if (sumOfDigits < 10) return sumOfDigits;
+    return getSumOfDigits(sumOfDigits)
 }
 
 module.exports = {
-  getSumOfDigits
+    getSumOfDigits
 };
