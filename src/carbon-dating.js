1a7aa43aa24daf7deb6b28bab5fca11b198bb666
Александр Попов <agpopov@t1-consulting.ru>
2023-04-17 13:41:58 +0300

    feat: resolve of carbon-dating, common-character-count and count-cats

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,4 +1,4 @@
-const {NotImplementedError} = require('../extensions/index.js');
+//const {NotImplementedError} = require('../extensions/index.js');
 
 const MODERN_ACTIVITY = 15;
 const HALF_LIFE_PERIOD = 5730;
