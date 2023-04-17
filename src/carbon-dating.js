88865e5bf60a8423e625106a941d8a91e61138c8
Александр Попов <agpopov@t1-consulting.ru>
2023-04-17 13:41:27 +0300

    feat: resolve of carbon-dating, common-character-count and count-cats

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const {NotImplementedError} = require('../extensions/index.js');
 
 const MODERN_ACTIVITY = 15;
 const HALF_LIFE_PERIOD = 5730;
@@ -6,22 +6,36 @@ const HALF_LIFE_PERIOD = 5730;
 /**
  * Determine the age of archeological find by using
  * given MODERN_ACTIVITY and HALF_LIFE_PERIOD values
- * 
- * @param {String} sampleActivity string representation of current activity 
+ *
+ * @param {String} sampleActivity string representation of current activity
  * @return {Number | Boolean} calculated age in years or false
  * in case of incorrect sampleActivity
  *
  * @example
- * 
+ *
  * dateSample('1') => 22387
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+    //throw new NotImplementedError('Not implemented');
+    if (typeof sampleActivity !== "string") return false;
+
+    const sampleActivityNumber = +sampleActivity;
+    const t = 0.693 / HALF_LIFE_PERIOD;
+    const kN0toN = Math.log(MODERN_ACTIVITY / sampleActivityNumber);
+
+    if (Math.ceil(kN0toN / t) <= 0) return false;
+
+    if (sampleActivityNumber > 0 && sampleActivityNumber <= MODERN_ACTIVITY) {
+        return Math.ceil(kN0toN / t);
+    } else {
+        return false;
+    }
 }
 
+dateSample('WOOT!')
+
 module.exports = {
-  dateSample
+    dateSample
 };
