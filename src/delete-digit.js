babc71df138967336bb3a02b97555efc546a046e
Александр Попов <agpopov@t1-consulting.ru>
2023-04-17 23:54:10 +0300

    feat: add solutions of delete-digit, dns-stats and encode-line.js

--- src/delete-digit.js
+++ src/delete-digit.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+//const {NotImplementedError} = require('../extensions/index.js');
 
 /**
  * Given some integer, find the maximal number you can obtain
@@ -11,11 +11,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function deleteDigit(n) {
+    //throw new NotImplementedError('Not implemented');
+    const arrFromN = n.toString().split("");
+    let maxNumber = +arrFromN[0];
+
+    for (let i = 0; i < arrFromN.length; i++) {
+        let arrTemp = arrFromN.concat();
+        arrTemp.splice(i, 1);
+
+        if (maxNumber < +arrTemp.join("")) maxNumber = +arrTemp.join("");
+    }
+    return maxNumber;
 }
 
 module.exports = {
-  deleteDigit
+    deleteDigit
 };
--- src/encode-line.js
+++ src/encode-line.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+//const { NotImplementedError } = require('../extensions/index.js');
 
 /**
  * Given a string, return its encoding version.
@@ -10,11 +10,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function encodeLine(str) {
+    //throw new NotImplementedError('Not implemented');
+    const arrFromStr = str.split("");
+    let count = 1;
+    const resultArr = [];
+
+    for (let i = 0; i < arrFromStr.length; i++) {
+        if (arrFromStr[i] !== arrFromStr[i + 1]) {
+            resultArr.push(count === 1 ? arrFromStr[i] : `${count}${arrFromStr[i]}`);
+            count = 1;
+        } else {
+            count++;
+        }
+    }
+    return resultArr.join("");
 }
 
 module.exports = {
-  encodeLine
+    encodeLine
 };
