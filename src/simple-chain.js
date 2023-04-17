84b8fd995f5d03cac9008aa1ea3766bb5fcca2e8
Александр Попов <agpopov@t1-consulting.ru>
2023-04-18 01:15:45 +0300

    feat: add solutions for recursive-depth, simple-chain and sort-by-height

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,32 +1,42 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const {NotImplementedError} = require('../extensions/index.js');
 
 /**
  * Implement chainMaker object according to task description
- * 
+ *
  */
 const chainMaker = {
-  getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    myChain: [],
+    getLength() {
+        //throw new NotImplementedError('Not implemented');
+        return this.myChain.length;
+    },
+    addLink(value) {
+        // throw new NotImplementedError('Not implemented');
+        this.myChain.push(`( ${value} )`);
+        return this;
+    },
+    removeLink(position) {
+        //throw new NotImplementedError('Not implemented');
+        if (!this.myChain[position - 1] || !Number.isInteger(position)) {
+            this.myChain = [];
+            throw new Error("You can't remove incorrect link!");
+        }
+        this.myChain.splice(position - 1, 1);
+        return this;
+    },
+    reverseChain() {
+        //throw new NotImplementedError('Not implemented');
+        this.myChain.reverse();
+        return this;
+    },
+    finishChain() {
+        //throw new NotImplementedError('Not implemented');
+        const result = this.myChain.join("~~");
+        this.myChain = [];
+        return result;
+    }
 };
 
 module.exports = {
-  chainMaker
+    chainMaker
 };
